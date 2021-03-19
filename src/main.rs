c18c14e106f570e3d699940b092ef79e2fbfc0e1
Tri Nguyen <kingpro068@gmail.com>
2021-03-19 21:56:58 +0700

    add flow subscribe with tokio task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use tokio::task;
+
 fn main() {
-    println!("Hello, world!");
+    const NATS_URL: &str = "http://localhost:4222";
+    const MAIL_SUBJ: &str = "nubes3_mail";
+
+    let nc = nats::connect(NATS_URL)?;
+    let sub = nc.subscribe(MAIL_SUBJ)?;
+    task::spawn(async {
+        if let Some(data) = sub.next().await? {
+            let msg = json!(data);
+            //Send mail func
+        }
+    });
 }
+
+
